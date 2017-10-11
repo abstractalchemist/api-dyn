0bae6ec6936d28d73f56da047b76a950c876ca8c
Jason Hirata <jhirata@integrity-apps.com>
2017-10-11 11:48:54 -1000

    added cache control

--- src/index.js
+++ src/index.js
@@ -5,16 +5,28 @@ const { fetchprice } = require('./app');
 const app = express();
 
 app.get('/price/:set/:id', function(req,res) {
-    let priceFetcher = fetchprice(req.params.set + "/" + req.params.id);
-    priceFetcher.subscribe(
-	price => {
-	    res.status(200).end(price);
-	},
-	_ => {
-	    res.send(404).end();
-	},
-	_ => {
-	})
+
+    console.log(`url => ${req.originalUrl}`);
+    let value = req.get('if-none-match');
+    console.log(`if-none-match => ${value}`);
+    if('foo' === value) {
+	return res.status(304).end()
+    }
+    else {
+    
+	res.append('cache-control','max-age=3600, must-revalidate, public');
+	res.append('etag','foo');
+	let priceFetcher = fetchprice(req.params.set + "/" + req.params.id);
+	priceFetcher.subscribe(
+	    price => {
+		res.status(200).end(price);
+	    },
+	    _ => {
+		res.send(404).end();
+	    },
+	    _ => {
+	    })
+    }
 })
 
 app.listen(8080);
